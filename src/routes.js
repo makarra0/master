48dc23b2373640d5b11c402400905727a366ceb0
makarra0 <marc0s.valverd3@gmail.com>
2022-03-09 15:22:23 -0300

    finalizando o CRUD de usuários

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,9 @@ const routes = express.Router()
 const UserController = require('./controllers/UserController')
 
 routes.get('/users', UserController.index)
+routes.post('/users', UserController.create)
+routes.put('/users/:id', UserController.update)
+routes.delete('/users/:id', UserController.delete)
+routes.get('/users/:id', UserController.getUser)
 
 module.exports = routes
