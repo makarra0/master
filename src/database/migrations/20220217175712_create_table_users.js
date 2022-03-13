e791cf70c37e337274d5ca3a29510c983d69e758
makarra0 <marc0s.valverd3@gmail.com>
2022-03-13 00:43:17 -0300

    Recriado estrutura migrate e seed

--- src/database/migrations/20220217175712_create_table_users.js
+++ src/database/migrations/20220217175712_create_table_users.js
@@ -4,7 +4,7 @@
  */
 exports.up = knex => knex.schema.createTable('users', table => {
     table.increments('id')
-    table.text('username') .unique().notNullable()
+    table.string('username', 255).unique().notNullable()
     
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
